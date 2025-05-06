61cf7df8a15da4a24f05a78036bb0d7afa1643c4
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-05-06 00:18:03 -0700

    Change method to form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    # @user = User.new(allowed_user_params) hash params from html form
 
     @user = User.new(allowed_user_params)
 
