cc035ce694b5f6765d400abebad4b9fb9d4a8ca3
Gregorius Avip <71865513+gregoriusavip@users.noreply.github.com>
2025-05-05 22:02:32 -0700

    Add method to create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,20 @@ class UsersController < ApplicationController
   def new
   end
   def create
+    # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    @user = User.new(allowed_user_params)
+
+    if @user.save
+      redirect_to new_users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def allowed_user_params
+    params.expect(user: [ :username, :email, :password ])
   end
 end
